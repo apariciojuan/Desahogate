83d9b00c8609c197efa72842ea59c65c73895206
Juan Jose Aparicio <32738434+apariciojuan@users.noreply.github.com>
2018-12-25 21:10:54 +0100

    id para view records

--- apps/entradas/urls.py
+++ apps/entradas/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', index_views.as_view(), name='index'),
     path('tags/<slug:tag_slug>/', tags_list, name='tags_list_view'),
     path('Records/', up_Record_views.as_view(), name='up_records'),
-    path('Record/<slug:slug>/', Record_views.as_view(), name='record_view'),
+    path('Record/<int:pk>/<slug:slug>/', Record_views.as_view(), name='record_view'),
     path('Record/Edit/<slug:slug>/', Record_edit_views.as_view(), name='record_edit_view'),
 ]
